6aed117e18c116efffb4fd949e240ac4ce9a3f50
xtuhcy <xtuhcy@163.com>
2017-07-23 20:55:14 +0800

    利用spring-boot的@Bean简化gecco的初始化

--- src/test/java/com/geccocrawler/gecco/spring/App.java
+++ src/test/java/com/geccocrawler/gecco/spring/App.java
@@ -2,10 +2,31 @@ package com.geccocrawler.gecco.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.geccocrawler.gecco.GeccoEngine;
 
 @SpringBootApplication
+@Configuration
 public class App {
 
+	@Bean
+	public SpringGeccoEngine initGecco() {
+		return new SpringGeccoEngine() {
+			@Override
+			public void init() {
+				GeccoEngine.create()
+				.pipelineFactory(springPipelineFactory)
+				.classpath("com.geccocrawler.gecco.spring")
+				.start("https://github.com/xtuhcy/gecco")
+				.interval(3000)
+				.loop(true)
+				.start();
+			}
+		};
+	}
+	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(App.class, args);
 	}
